e4c4ace2f54a3eb26879f9b9a2ec0e6b8c4d26ec
Kevin Schenk <kevin@kschenk.com>
2016-02-15 10:45:04 -0700

    Top node added startpoint

--- convo.js
+++ convo.js
@@ -3,31 +3,31 @@ var dialogueRoot;
 var idCounter = 0;
 
 function buildRecursiveAddToBody(dialogue) {
-    var startpointOptions = { 
-        isSource:true, 
-        endpoint: ["Dot", {radius:7}], 
-        connector: ["Flowchart"], 
-        connectorStyle: { 
+    var startpointOptions = {
+        isSource:true,
+        endpoint: ["Dot", {radius:7}],
+        connector: ["Flowchart"],
+        connectorStyle: {
             strokeStyle: "black",
             lineWidth: 3,
-        }, paintStyle: { 
+        }, paintStyle: {
             fillStyle:"black"
-        }, maxConnections: 999999 
+        }, maxConnections: 999999
     };
-    
+
     var endpointOptions = Object.create(startpointOptions);
-    
+
     endpointOptions.isSource = false;
     endpointOptions.isTarget = true;
-    
+
     var node = $("<div/>", {
         class: "dialogue-node",
         id: "dialogue-node-" + dialogue.id,
         style: "left: " + dialogue.x + "px; top: " + dialogue.y + "px",
     }).appendTo("#convo-editor");
-    
+
     node.text(dialogue.text);
-    
+
     jsPlumb.draggable(node, {
         stop: function(event, ui) {
             // Write to dialogue.
@@ -35,38 +35,51 @@ function buildRecursiveAddToBody(dialogue) {
             dialogue.y = node.position().top;
         }
     });
-    
+
     setTimeout(function() {
         var startpoint = jsPlumb.addEndpoint(node, { anchor: "BottomCenter"}, startpointOptions);
-    
+        var endpoint = jsPlumb.addEndpoint(node, { anchor: "TopCenter"}, endpointOptions);
+
+        node.startpoint = startpoint;
+        node.endpoint = endpoint;
+
         $.each(dialogue.responses, function(key, value) {
             var child = buildRecursiveAddToBody(value);
 
             setTimeout(function() {
-                var endpoint = jsPlumb.addEndpoint(child, { anchor: "TopCenter"}, endpointOptions);
-                jsPlumb.connect({source: startpoint, target: endpoint});
+                jsPlumb.connect({source: startpoint, target: child.endpoint});
             }, 0);
         });
     });
-    
+
     return node;
 }
 
 
 $(function() {
+    // jsPlumb.Defaults.Overlays = [
+    //     [ "Arrow", {
+    //         location:1,
+    //         id:"arrow",
+    //         length:14,
+    //         foldback:1.0
+    //     } ],
+    //     [ "Arrow", {
+    //         location:2,
+    //         id:"arrow-2",
+    //         length:14,
+    //         foldback:1.0
+    //     } ],
+    // ];
     $.getJSON("test-dialogue.json", function( data ) {
         dialogueRoot = data;
-        
+
          $.each(dialogueRoot.dialogues, function(key, value) {
              buildRecursiveAddToBody(value);
-         }); 
+         });
     });
-    
+
     $("#export-json").click(function() {
-//        var dialogueExport = {
-//            dialogues: dialogueRoot
-//        };
-//        
         $("<a />", {
             "download": "data.json",
             "href" : "data:application/json," + encodeURIComponent(JSON.stringify(dialogueRoot, null, 2))
